a12c51f934e8752c8c3ae47280942c4b94ff464a
sharanagouda.k <sharanagouda.k@robosoftin.com>
2020-12-03 10:36:19 +0530

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './src/AppNavigator';
+import NavigationService from './src/NavigationService';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppNavigator
+      ref={navigatorRef => {
+        NavigationService.setTopLevelNavigator(navigatorRef);
+      }}
+      uriPrefix={'demo://'}
+    />
   );
 }
 
---
+++ src/Second.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+import { View, StatusBar, StyleSheet, Text, FlatList } from 'react-native';
+
+class Second extends Component {
+    static navigationOptions = {
+        headerTintColor: 'white',
+        headerStyle: {
+            backgroundColor: '#f76c57',
+        },
+    };
+
+    async componentDidMount() {
+        const { navigation } = this.props;
+
+        // try calling demo://home/second/10 in simulator's browser
+
+        // if (navigation.state.params?.secondId)
+        if (navigation.state.params.secondId)
+            this.flatListRef.scrollToIndex({
+                animated: true,
+                index: navigation.state.params.secondId,
+            });
+    }
+
+    render() {
+        const { navigation } = this.props;
+
+        return (
+            <View>
+                <StatusBar barStyle="light-content" />
+                <View style={styles.container}>
+                    <Text style={styles.title}>Second level</Text>
+                    {navigation.state.params && (
+                        <Text style={[styles.subtitle, styles.params]}>
+                            Param passed in DL: {navigation.state.params.secondId}
+                        </Text>
+                    )}
+                    <FlatList
+                        data={Array.from({ length: 50 }, (_, k) => k + 1)}
+                        ref={ref => {
+                            this.flatListRef = ref;
+                        }}
+                        keyExtractor={item => item.id}
+                        renderItem={(data, i) => (
+                            <View key={data.item}>
+                                <Text style={styles.subtitle}>
+                                    item no -- {data.item}
+                                </Text>
+                            </View>
+                        )}
+                        onScrollToIndexFailed={error => {
+                            this.flatListRef.scrollToOffset({
+                                offset: error.averageItemLength * error.index,
+                                animated: true,
+                            });
+                            setTimeout(() => {
+                                if (this.flatListRef !== null) {
+                                    this.flatListRef.scrollToIndex({
+                                        index: error.index,
+                                        animated: true,
+                                    });
+                                }
+                            }, 100);
+                        }}
+                    />
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 100,
+        alignItems: 'center',
+        backgroundColor: '#0f293b',
+        height: '100%',
+        padding: 30,
+    },
+    title: {
+        marginTop: 50,
+        fontFamily: 'Verdana',
+        fontSize: 40,
+        color: 'white',
+    },
+    subtitle: {
+        marginTop: 10,
+        fontFamily: 'Verdana',
+        fontSize: 20,
+        color: 'white',
+    },
+    params: {
+        marginBottom: 20,
+    },
+});
+
+export default Second;
